b2d57cb33bd730df688fce820ed913b2039f49dd
jacobmiller22 <jacobmiller22@vt.edu>
2022-05-21 14:09:11 -0400

    ItemsView displays locations' names rather than locationIds

--- client/src/views/Items/ItemsView/ItemsView.tsx
+++ client/src/views/Items/ItemsView/ItemsView.tsx
@@ -1,23 +1,43 @@
 import { useState, useEffect } from "react";
 import styles from "./IndexView.module.css";
 import { DataTable } from "components";
-import { deleteItems, getItems as __getItems } from "api/inv";
+import {
+  deleteItems,
+  getItems as __getItems,
+  getLocations as __getLocations,
+} from "api/inv";
 import { ItemId, MinItem } from "interfaces/item";
 import { Tag } from "interfaces/tag";
 import { itemDetailsRoute, newItemRoute } from "Router/routes/client";
 import { replaceWildcards } from "Router/routes";
 import { useNavigate } from "react-router-dom";
+import { LocationId, MinLocation } from "interfaces/location";
+
+type Map<V> = { [key: string]: V };
 
 const ItemsView = () => {
   const navigate = useNavigate();
   const [items, setItems] = useState<MinItem[] | null>(null);
+  const [locationMap, setLocationMap] = useState<Map<string> | null>(null);
 
   const getItems = async () => {
     setItems(await __getItems());
   };
 
+  const getLocationMap = async () => {
+    const locations = (await __getLocations()) ?? [];
+    let _locationMap: Map<string> = {};
+    locations.forEach(
+      (location: MinLocation) =>
+        (_locationMap[location.locationId] = location.name)
+    );
+
+    setLocationMap(_locationMap);
+  };
+
   useEffect(() => {
     getItems();
+    getLocationMap();
   }, []);
 
   const handleCellDoubleClick = (cell: { id: ItemId }) => {
@@ -27,9 +47,20 @@ const ItemsView = () => {
   const handleDelete = async (itemIds: ItemId[]): Promise<boolean> =>
     await deleteItems(itemIds);
 
+  const mappedItems = items?.map(
+    (item: Omit<MinItem, "locationId"> & { locationId?: LocationId }) => {
+      const newItem = {
+        ...item,
+        location: locationMap?.[item.locationId!] as string,
+      };
+      delete newItem.locationId;
+      return newItem;
+    }
+  );
+
   const dataTableProps = {
     idKey: "itemId",
-    items,
+    items: mappedItems,
     columns,
     onCellDoubleClick: handleCellDoubleClick,
     title: "Items",
@@ -41,6 +72,7 @@ const ItemsView = () => {
   return (
     <div>
       <br />
+      {/* @ts-expect-error */}
       <DataTable {...dataTableProps} />
     </div>
   );
@@ -70,7 +102,7 @@ const columns = [
     editable: false,
   },
   {
-    field: "locationId",
+    field: "location",
     headerName: "Location",
     width: 200,
     editable: false,
